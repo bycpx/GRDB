5a6274d3f8e0ea10d9b5274ff67362b2ff931a8f
Christoph Petrick <xlii@gmx.de>
2012-06-30 16:29:47 +0200

    Fix wrong online status taken from messages

--- inject.js
+++ inject.js
@@ -21,13 +21,26 @@ function visitorDisplayChanged(event)
 	}
 }
 
+function userSeen(element)
+{
+	var stat = 0;
+	if((st = element.nextElementSibling.nextElementSibling) && st.className) {
+	    stat = st.className.slice(-1)=="0"?1:2;
+	} else if(element.nextElementSibling.textContent.length>1) {
+	    stat = -2;
+	} else {
+	    stat = -1;
+	}
+	msg = [el.className.slice(4), stat];
+	safari.self.tab.dispatchMessage("userSeen", msg);
+}
+
 if(window.top===window) {
 	if(window.location.href && window.location.href.indexOf("/logout/")!=-1) {
 		safari.self.tab.dispatchMessage("sessionDidEnd");
 	}
 	if(window.location.href && window.location.href.indexOf("/msg/")!=-1 && (el=document.getElementById("userName"))) {
-		msg = [el.className.slice(4), el.nextElementSibling.nextElementSibling ? el.nextElementSibling.nextElementSibling.className.slice(-1) : "--"];
-		safari.self.tab.dispatchMessage("userSeen", msg);
+		userSeen(el);
 	}
 
 } else {
